1ea582d2260bbf8467601c6c26d807d69ba654cb
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-12-19 11:41:57 -0300

    project initialized

---
+++ api/src/main/java/com/insight/challenge/insighttrackingplatform/InsightTrackingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.insight.challenge.insighttrackingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightTrackingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsightTrackingPlatformApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/insight/challenge/insighttrackingplatform/InsightTrackingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insight.challenge.insighttrackingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightTrackingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
