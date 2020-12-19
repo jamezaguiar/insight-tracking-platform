3fe406ae42deeb4e5a36b353d3bfa80335d128c5
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-12-19 16:32:22 -0300

    User DELETE and PUT methods

--- api/src/main/java/com/insight/challenge/insighttrackingplatform/resources/UserResource.java
+++ api/src/main/java/com/insight/challenge/insighttrackingplatform/resources/UserResource.java
@@ -3,11 +3,14 @@ package com.insight.challenge.insighttrackingplatform.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.insight.challenge.insighttrackingplatform.models.User;
@@ -32,6 +35,17 @@ public class UserResource {
 
 	@PostMapping("/users")
 	public User registerUser(@RequestBody User user) {
+		return usersRepository.save(user);
+	}
+
+	@DeleteMapping("/users")
+	public void deleteUser(@RequestParam long user_id) {
+		User user = usersRepository.findById(user_id);
+		usersRepository.delete(user);
+	}
+
+	@PutMapping("/users")
+	public User updateUser(@RequestBody User user) {
 		return usersRepository.save(user);
 	}
 
