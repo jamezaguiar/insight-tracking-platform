1383330410fd52f60253a6a7b6a02c74862fe3c5
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-12-19 16:06:33 -0300

    User GET and POST methods

---
+++ api/src/main/java/com/insight/challenge/insighttrackingplatform/resources/UserResource.java
@@ -0,0 +1,38 @@
+package com.insight.challenge.insighttrackingplatform.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.insight.challenge.insighttrackingplatform.models.User;
+import com.insight.challenge.insighttrackingplatform.repositories.UsersRepository;
+
+@RestController
+@RequestMapping(value = "/api")
+public class UserResource {
+
+	@Autowired
+	UsersRepository usersRepository;
+
+	@GetMapping("/users")
+	public List<User> listUsers() {
+		return usersRepository.findAll();
+	}
+
+	@GetMapping("/users/{id}")
+	public User listUserById(@PathVariable(value = "id") long id) {
+		return usersRepository.findById(id);
+	}
+
+	@PostMapping("/users")
+	public User registerUser(@RequestBody User user) {
+		return usersRepository.save(user);
+	}
+
+}
