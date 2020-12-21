b149e997fc1c6de5f38a3db9493f1adb4f4323b2
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-12-21 10:50:10 -0300

    Global style and Roboto font

---
+++ web/src/styles/global.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  body {
+    background: #152438;
+    color: #fff;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
