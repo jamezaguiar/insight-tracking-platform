4f4e4a292061de1c0328fc5cb7b86f3c7ca3974e
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-12-21 18:30:13 -0300

    Dashboard page structure and styles finalized

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,15 +1,57 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { FiPlus, FiSearch } from 'react-icons/fi';
+
+import {
+  Container,
+  LogoContainer,
+  Options,
+  CandidatesContainer,
+  Candidate,
+} from './styles';
 
 const Dashboard: React.FC = () => {
   return (
     <Container>
-      <img
-        src="https://insightlab.ufc.br/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2020/02/LogoInsight.png.webp"
-        alt="Insight Logo"
-      />
-      <h1>Dashboard</h1>
+      <LogoContainer>
+        <img
+          src="https://insightlab.ufc.br/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2020/02/LogoInsight.png.webp"
+          alt="Insight Logo"
+        />
+        <h1>Tracking Platform</h1>
+      </LogoContainer>
+      <Options>
+        <span>
+          <FiPlus size={24} />
+          Cadastrar candidato
+        </span>
+        <span>
+          <FiSearch size={24} />
+          Buscar candidatos por atividade
+        </span>
+      </Options>
+      <CandidatesContainer>
+        <Candidate>
+          <div>
+            <strong>Jamerson Aguiar</strong>
+            <p>jamersonalsilva14@gmail.com</p>
+          </div>
+          <div>
+            <button type="button">Ver atividades</button>
+            <button type="button">Excluir</button>
+          </div>
+        </Candidate>
+        <Candidate>
+          <div>
+            <strong>Jamerson Aguiar</strong>
+            <p>jamersonalsilva14@gmail.com</p>
+          </div>
+          <div>
+            <button type="button">Ver atividades</button>
+            <button type="button">Excluir</button>
+          </div>
+        </Candidate>
+      </CandidatesContainer>
     </Container>
   );
 };
--- web/src/pages/Dashboard/styles.ts
+++ web/src/pages/Dashboard/styles.ts
@@ -1,3 +1,112 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  font-size: 14px;
+
+  margin-top: 32px;
+
+  img {
+    width: 200px;
+  }
+
+  h1 {
+    font-weight: 100;
+  }
+`;
+
+export const Options = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  margin-top: 64px;
+
+  font-size: 24px;
+
+  span {
+    display: flex;
+    align-items: center;
+    margin: 16px;
+
+    transition: color 0.2s;
+
+    cursor: pointer;
+
+    &:hover {
+      color: ${shade(0.2, '#ddd')};
+    }
+
+    svg {
+      margin-right: 8px;
+    }
+  }
+`;
+
+export const CandidatesContainer = styled.div`
+  margin-top: 80px;
+`;
+
+export const Candidate = styled.div`
+  width: 95vw;
+  padding: 24px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  background-color: #ddd;
+
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: ${shade(0.05, '#ddd')};
+  }
+
+  border-radius: 2px;
+
+  & + div {
+    margin-top: 16px;
+  }
+
+  strong {
+    color: #000;
+  }
+
+  p {
+    color: #222;
+  }
+
+  div {
+    button {
+      width: 120px;
+      height: 40px;
+
+      border: none;
+
+      margin: 8px;
+
+      color: #eee;
+      background-color: #007fff;
+
+      transition: background-color 0.2s;
+
+      &:hover {
+        background-color: ${shade(0.2, '#007fff')};
+      }
+    }
+  }
+`;
