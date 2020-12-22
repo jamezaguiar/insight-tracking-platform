910b57e7f95cdf137e90e9a015881dddad528e88
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-12-22 20:36:49 -0300

    Find candidates by activity page

--- web/src/pages/FindCandidates/index.tsx
+++ web/src/pages/FindCandidates/index.tsx
@@ -1,11 +1,119 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FiChevronLeft, FiSearch } from 'react-icons/fi';
 
-import { Container } from './styles';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+
+import { useToast } from '../../hooks/toast';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import {
+  Container,
+  SearchContainer,
+  CandidatesContainer,
+  Candidate,
+} from './styles';
+import api from '../../services/api';
+
+interface FindCandidatesFormData {
+  activity_name: string;
+}
+
+interface Candidate {
+  id: string;
+  name: string;
+  email: string;
+}
 
 const FindCandidates: React.FC = () => {
+  const [candidates, setCandidates] = useState<Candidate[]>([]);
+  const [activity, setActivity] = useState('');
+
+  const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+
+  const handleSubmit = useCallback(async (data: FindCandidatesFormData) => {
+    const response = await api.get<Candidate[]>(
+      `/candidates/activities?activity_name=${data.activity_name}`,
+    );
+
+    if (response.data.length === 0) {
+      addToast({
+        type: 'info',
+        title: 'Não encontrado',
+        description: 'Não foram encontrados candidatos com essa atividade.',
+      });
+    }
+
+    setActivity(data.activity_name);
+    setCandidates(response.data);
+  }, []);
+
+  const handleDeleteCandidate = useCallback(
+    async (candidate_id: string) => {
+      try {
+        await api.delete(`/candidates?candidate_id=${candidate_id}`);
+        handleSubmit({ activity_name: activity });
+        addToast({
+          title: 'Feito!',
+          type: 'success',
+          description: 'Candidato excluído com sucesso!',
+        });
+      } catch (err) {
+        addToast({
+          title: 'Algo deu errado.',
+          type: 'error',
+          description: 'Não foi possível excluir o candidato, tente novamente.',
+        });
+      }
+    },
+    [activity, addToast, handleSubmit],
+  );
+
   return (
     <Container>
-      <h1>FindCandidates</h1>
+      <h1>Buscar candidatos</h1>
+      <Link to="/">
+        <FiChevronLeft size={24} />
+        Voltar
+      </Link>
+      <SearchContainer>
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <Input
+            name="activity_name"
+            placeholder="Digite o nome da atividade"
+            icon={FiSearch}
+          />
+          <Button type="submit">Buscar</Button>
+        </Form>
+      </SearchContainer>
+      <CandidatesContainer>
+        {candidates.map((candidate) => (
+          <Candidate key={candidate.id}>
+            <div>
+              <strong>{candidate.name}</strong>
+              <p>{candidate.email}</p>
+            </div>
+            <div>
+              <Link to={`activities/${candidate.id}`}>
+                <Button type="button">Ver atividades</Button>
+              </Link>
+              <Link to={`edit-candidate/${candidate.id}`}>
+                <Button type="button">Editar</Button>
+              </Link>
+              <Button
+                type="button"
+                onClick={() => handleDeleteCandidate(candidate.id)}
+              >
+                Excluir
+              </Button>
+            </div>
+          </Candidate>
+        ))}
+      </CandidatesContainer>
     </Container>
   );
 };
--- web/src/pages/FindCandidates/styles.ts
+++ web/src/pages/FindCandidates/styles.ts
@@ -1,3 +1,81 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  > h1 {
+    margin: 80px 0;
+  }
+
+  > a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    text-decoration: none;
+    color: #ddd;
+
+    transition: color 0.2s;
+
+    &:hover {
+      color: ${shade(0.2, '#ddd')};
+    }
+
+    font-size: 24px;
+
+    margin-bottom: 32px;
+  }
+`;
+
+export const SearchContainer = styled.div`
+  form {
+    display: flex;
+    align-items: center;
+
+    input {
+      width: 75vw;
+    }
+  }
+  button {
+    width: 150px;
+    height: 54px;
+    margin: 0 0 0 8px;
+  }
+`;
+
+export const CandidatesContainer = styled.div`
+  margin-top: 80px;
+`;
+
+export const Candidate = styled.div`
+  width: 95vw;
+  padding: 24px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  background-color: #ddd;
+
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: ${shade(0.05, '#ddd')};
+  }
+
+  border-radius: 2px;
+
+  margin-bottom: 16px;
+
+  strong {
+    color: #000;
+  }
+
+  p {
+    color: #222;
+  }
+`;
